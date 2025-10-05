9dea205aaf0c8067dec6907c98aa76b8bd8a3688
Yahia <yahiaxe300@gmail.com>
2025-10-06 02:36:19 +0300

    Initialize

---
+++ src/main/java/com/example/encore/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.encore;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
