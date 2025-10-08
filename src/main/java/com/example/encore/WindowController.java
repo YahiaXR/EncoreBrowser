7676b86207417090f6a87180880904bce6f8dc79
Yahia <yahiaxe300@gmail.com>
2025-10-09 02:58:36 +0300

    More JavaFX Tweaks

--- src/main/java/com/example/encore/WindowController.java
+++ src/main/java/com/example/encore/WindowController.java
@@ -1,4 +1,36 @@
 package com.example.encore;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+
+import java.awt.event.ActionEvent;
+
 public class WindowController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(EncoreApp.class.getResource("EncoreMain.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void switchToScene2(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(EncoreApp.class.getResource("EncoreMain.fxml"));
+        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.encore {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.encore to javafx.fxml;
     exports com.example.encore;
